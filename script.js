97974884c3f00f70a8f7c71f32e1b7cc6991466a
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-07-07 16:52:00 +0100

    create file with syntax for rock, paper and scissors game

---
+++ script.js
@@ -0,0 +1,20 @@
+function getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    // your code goes here!
+         return "You Lose! Paper beats Rock"
+}
+
+// give 'playerSelection' a value of 'rock'.
+// give 'computerSelection' the getComputerChoice function.
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+//function for the actual game goes here.
+function game() {
+  function playRound(playerSelection, computerSelection) {
+    // your code goes here!
+    return "You Lose! Paper beats Rock"
+  }
+}
